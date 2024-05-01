c5a76a14e69899a17270efa8787aceea09afb046
when-harry-met-sally <milesmoran.mu@gmail.com>
2024-05-01 16:30:43 -0500

    saving

--- index.sh
+++ index.sh
@@ -1,29 +1,36 @@
-function my_git() {
-	local original_command="git"
+#!/bin/zsh
+
+function git() {
+	local original_command="/usr/bin/git"
 	local current_dir="$PWD"
-	local config_file="${XDG_CONFIG_HOME:-$HOME/.config}/git-outta-here" # Path to the directory mappings config file
-	local git_config="$HOME/.gitconfig"                                  # Default Git config
+	local config_file="${XDG_CONFIG_HOME:-$HOME/.config}/git-outta-here.json"
+	local git_config="$HOME/.gitconfig"
 
-	local ORANGE="\033[38;5;208m" # ANSI 256-color code for orange
+	local ORANGE="\033[38;5;208m"
 	local BLUE="\033[0;34m"
-	local NC="\033[0m" # No Color
+	local NC="\033[0m"
 
-	# Read mappings from config file and set the appropriate Git config
-	while IFS=' ' read -r dir_pattern config_path; do
+	# Load the default git configuration
+	git_config=$(jq -r '.directories[] | select(.path=="/*") | .config' "$config_file")
+	git_config="${git_config/#\~/$HOME}"
+
+	# Find the specific git configuration for the current directory
+	while IFS= read -r line; do
+		dir_pattern=$(echo "$line" | jq -r '.path')
+		config_path=$(echo "$line" | jq -r '.config')
+
 		# Manually expand tilde to home directory
 		config_path="${config_path/#\~/$HOME}"
-		# echo "Read pattern: $dir_pattern with config: $config_path" # Debug output
-		if [[ -n "$dir_pattern" && "$current_dir" == *"$dir_pattern"* ]]; then
-			git_config="$config_path"
-			# echo "Match found: $dir_pattern setting config to $config_path" # Debug output
-			break # Stop at the first matching pattern
-		fi
-	done <"$config_file"
 
-	# Set the global Git configuration environment variable
+		if [[ "$current_dir" == *"$dir_pattern"* ]]; then
+			git_config="$config_path"
+			break
+		fi
+	done < <(jq -c '.directories[]' "$config_file")
+
 	export GIT_CONFIG_GLOBAL="$git_config"
 
-	echo -e "${ORANGE}[git-outta-here]${NC} ${BLUE}$GIT_CONFIG_GLOBAL${NC}"
+	echo -e "${ORANGE}[git-outta-here]${NC}${BLUE}$GIT_CONFIG_GLOBAL${NC}"
 	echo ""
 
 	# Execute the Git command with the dynamically set global configuration

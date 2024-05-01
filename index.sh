24b53b918b24a5608fd3d13d485b91dbc5291619
when-harry-met-sally <milesmoran.mu@gmail.com>
2024-05-01 17:06:14 -0500

    allowing for a debug mode to ignore logs

--- index.sh
+++ index.sh
@@ -1,38 +1,44 @@
 #!/bin/zsh
 
 function git() {
-	local original_command="/usr/bin/git"
-	local current_dir="$PWD"
-	local config_file="${XDG_CONFIG_HOME:-$HOME/.config}/git-outta-here.json"
-	local git_config="$HOME/.gitconfig"
+    local original_command="/usr/bin/git"
+    local current_dir="$PWD"
+    local config_file="${XDG_CONFIG_HOME:-$HOME/.config}/git-outta-here.json"
+    local git_config="$HOME/.gitconfig"
 
-	local ORANGE="\033[38;5;208m"
-	local BLUE="\033[0;34m"
-	local NC="\033[0m"
+    local ORANGE="\033[38;5;208m"
+    local BLUE="\033[0;34m"
+    local NC="\033[0m"
 
-	# Load the default git configuration
-	git_config=$(jq -r '.directories[] | select(.path=="/*") | .config' "$config_file")
-	git_config="${git_config/#\~/$HOME}"
+    # Fetch the debug setting from the JSON configuration
+    local debug=$(jq -r '.debug // false' "$config_file")
 
-	# Find the specific git configuration for the current directory
-	while IFS= read -r line; do
-		dir_pattern=$(echo "$line" | jq -r '.path')
-		config_path=$(echo "$line" | jq -r '.config')
+    # Load the default git configuration
+    git_config=$(jq -r '.directories[] | select(.path=="/*") | .config' "$config_file")
+    git_config="${git_config/#\~/$HOME}"
 
-		# Manually expand tilde to home directory
-		config_path="${config_path/#\~/$HOME}"
+    # Find the specific git configuration for the current directory
+    while IFS= read -r line; do
+        dir_pattern=$(echo "$line" | jq -r '.path')
+        config_path=$(echo "$line" | jq -r '.config')
 
-		if [[ "$current_dir" == *"$dir_pattern"* ]]; then
-			git_config="$config_path"
-			break
-		fi
-	done < <(jq -c '.directories[]' "$config_file")
+        # Manually expand tilde to home directory
+        config_path="${config_path/#\~/$HOME}"
 
-	export GIT_CONFIG_GLOBAL="$git_config"
+        if [[ "$current_dir" == *"$dir_pattern"* ]]; then
+            git_config="$config_path"
+            break
+        fi
+    done < <(jq -c '.directories[]' "$config_file")
 
-	echo -e "${ORANGE}[git-outta-here]${NC}${BLUE}$GIT_CONFIG_GLOBAL${NC}"
-	echo ""
+    export GIT_CONFIG_GLOBAL="$git_config"
 
-	# Execute the Git command with the dynamically set global configuration
-	command $original_command "$@"
+    # Conditionally display debug information
+    if [[ "$debug" == true ]]; then
+        echo -e "${ORANGE}[git-outta-here]${NC}${BLUE}$GIT_CONFIG_GLOBAL${NC}"
+        echo ""
+    fi
+
+    # Execute the Git command with the dynamically set global configuration
+    command $original_command "$@"
 }
